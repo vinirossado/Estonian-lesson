43ab7abfaea60f7db292bdd48879d342c79f6304
Vinicius Rossado <vinicius.rossado@riverty.com>
2025-02-06 19:45:54 +0200

    Fixing tailwind import

--- estonian/src/components/LanguageSwitcher.tsx
+++ estonian/src/components/LanguageSwitcher.tsx
@@ -4,7 +4,7 @@ export const LanguageSwitcher = () => {
     const { language, setLanguage } = useTranslation();
 
     return (
-        <div class="container">
+        <div >
             {language === 'en' ? (
                 <button
                     class="bg-white text-gray-700 font-semibold py-2 px-4 "
